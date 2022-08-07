3e861b73b3c68a6992c8032e7e7224316c9bfee1
yeongseokim <1903020@ewhain.net>
2022-08-07 23:15:34 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React from "react";
+import { useState } from "react";
 import './App.css';
+import Form from "./components/Form";
+import Lists from "./components/Lists";
 
-function App() {
+const initialTodoData = localStorage.getItem("todoData") ? JSON.parse(localStorage.getItem("todoData")) : [];
+
+export default function App() {
+  console.log("App Component");
+
+  const [todoData, setTodoData] = useState(initialTodoData); //처음에는 빈 배열 사용
+  const [value, setValue] = useState(""); //처음에는 빈 스트링
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); //페이지 리로드 막기
+    let newTodo = {
+      id: Date.now(), //아이디가 유니크한 값이어야 해서 이렇게 작성
+      title: value, //handleChange에 의해 현재 밸류가 여기에 들어있다.
+      completed: false,
+    }
+    setTodoData(prev => [...prev, newTodo]); //Setter에서 이전 state를 가져오려면 인수에 함수를 이용
+    localStorage.setItem('todoData', JSON.stringify([...todoData, newTodo]));
+    setValue("");
+
+  }
+
+  const handleRemoveClick = () => {
+    setTodoData([]);
+    localStorage.setItem('todoData', JSON.stringify([]));
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
+      <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg">
+        <div className="flex justify-between mb-3">
+          <h1>할 일 목록</h1>
+          <button onClick={handleRemoveClick}>Delete All</button>
+        </div>
+        <Lists todoData={todoData} setTodoData={setTodoData} />
+        <Form value={value} setValue={setValue} handleSubmit={handleSubmit} />
+      </div>
     </div>
   );
-}
 
-export default App;
+}
---
+++ src/components/List.js
@@ -0,0 +1,102 @@
+import React, { useState } from 'react'
+
+const List = React.memo(({
+    id,
+    title,
+    completed,
+    todoData,
+    setTodoData,
+    provided,
+    snapshot
+}) => {
+
+    const [isEditing, setIsEditing] = useState(false);
+    const [editedTitle, setEditedTitle] = useState(title);
+
+    const handleCompleChange = (id) => {
+        let newTodo = todoData.map((data) => {
+            if (data.id === id) {
+                data.completed = !completed; //not 연산
+            }
+            return data;
+        })
+        setTodoData(newTodo);
+        localStorage.setItem('todoData', JSON.stringify(newTodo));
+
+    }
+
+    const handleClick = (id) => {
+        let newTodoData = todoData.filter(data => data.id !== id); //x버튼을 누른 엘리먼트를 제외한 새로운 요소들
+        setTodoData(newTodoData);
+        localStorage.setItem('todoData', JSON.stringify(newTodoData));
+    }
+
+    const handleEditChange = (event) => {
+        setEditedTitle(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        let newTodoData = todoData.map(data => {
+            if (data.id === id) {
+                data.title = editedTitle;
+            }
+            return data;
+        })
+        setTodoData(newTodoData);
+        localStorage.setItem('todoData', JSON.stringify(newTodoData));
+
+        setIsEditing(false);
+    }
+
+    if (isEditing) {
+        return (
+            <div className='flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600  bg-gray-100 border rounded'>
+                <div className='items-center'>
+                    <form onSubmit={handleSubmit}>
+                        <input
+                            value={editedTitle}
+                            onChange={handleEditChange}
+                            className="w-full px-3 py-2 mr-4 text-gray-500 rounded"
+
+                        />
+                    </form>
+                </div >
+                <div className='items-center'>
+                    <button className='px-4 py-2 float-right' onClick={() => setIsEditing(false)}>x</button>
+                    <button className='px-4 py-2 float-right' typy="submit" onClick={handleSubmit}>save</button>
+                </div>
+            </div >
+
+        );
+    } else {
+
+        return (
+            <div
+                key={id}
+                {...provided.draggableProps}
+                ref={provided.innerRef}
+                {...provided.dragHandleProps}
+                className={snapshot.isDragging ? "selected" : "not-selected"}
+            >
+                <div className={`${snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"} flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600  bg-gray-100 border rounded`}>
+                    <div className='items-center'>
+                        <input
+                            type="checkbox"
+                            onChange={() => handleCompleChange(id)}
+                            defaultChecked={false} />
+                        <span
+                            className={completed ? "line-through" : undefined}
+                        > {title}</span>
+                    </div>
+                    <div className='items-center'>
+                        <button className='px-4 py-2 float-right' onClick={() => handleClick(id)}>x</button>
+                        <button className='px-4 py-2 float-right' onClick={() => setIsEditing(true)}>edit</button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+});
+
+export default List
